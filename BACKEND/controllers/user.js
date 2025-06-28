1e46eb76771dcd1ee2e902fa9051cd96cc96a6ed
ignaccio7 <nacho260298@gmail.com>
2025-06-28 19:52:24 -0400

    fix(backend): lint and return avatar in login user

--- BACKEND/controllers/user.js
+++ BACKEND/controllers/user.js
@@ -1,26 +1,27 @@
+import bcrypt from 'bcrypt'
 import { generateToken } from '../middlewares/authJWT.js'
 import UserModel from '../models/user.js'
 import { validatePerson } from '../schemas/person.js'
 import { validatePartialUser, validateUser } from '../schemas/user.js'
 import { deleteLocalImage, uploadImage } from '../utils/utils.js'
-import bcrypt from 'bcrypt'
 
 // TODO en esta tabla falta añadir el rol al momento de registrar y/o modificar
 
 export default class UserController {
-  static async getAll (request, response) {
+  // static async getAll(request, response) {
+  static async getAll(_, response) {
     try {
       const results = await UserModel.getAll()
       return response.json({
         statusCode: 200,
         message: 'Solicitud exitosa',
-        data: results
+        data: results,
       })
     } catch (error) {
       console.log(error)
       response.status(500).json({
         statusCode: 500,
-        message: 'Fallo al solicitar datos a la tabla user'
+        message: 'Fallo al solicitar datos a la tabla user',
       })
     }
   }
@@ -78,7 +79,7 @@ export default class UserController {
   }
   */
   // Register user with Image
-  static async signup (request, response) {
+  static async signup(request, response) {
     // const { ci, nombres, paterno, materno, telefono, correo, usuario, pass, rol } = request.body
     const body = request.body
     const file = request.file
@@ -96,27 +97,31 @@ export default class UserController {
 
     if (resultUser.error || resultPerson.error) {
       let errorMessage = resultUser?.error?.issues[0]?.message ?? null
-      errorMessage = errorMessage ?? resultPerson?.error?.issues[0]?.message ?? 'Error en los campos ingresados'
+      errorMessage =
+        errorMessage ?? resultPerson?.error?.issues[0]?.message ?? 'Error en los campos ingresados'
 
       console.log({ errorMessage })
 
       return response.status(422).json({
         statusCode: 422,
-        message: errorMessage
+        message: errorMessage,
         // {User: resultUser.error,
         // Person: resultPerson.error}
-
       })
     }
     // TODO: Verificar que si existe errores al subir imagenes nose guarden en LOCAL esto con desarrollo o en produccion veriamos la falla en el peor de los casos XD
     // Probar que el ssitema nose rompa ya que el frontend cambia ya que le estamos enviando el rol del usuario
 
-    const isUserExist = await UserModel.isExistUser({ ci: resultPerson.data.ci, username: resultUser.data.usuario })
+    const isUserExist = await UserModel.isExistUser({
+      ci: resultPerson.data.ci,
+      username: resultUser.data.usuario,
+    })
     if (isUserExist.length !== 0) {
       if (file) await deleteLocalImage({ file })
       return response.status(409).json({
         statusCode: 409,
-        message: 'Usted ya cuenta con un usuario registrado o El nombre de usuario ya esta ocupado.'
+        message:
+          'Usted ya cuenta con un usuario registrado o El nombre de usuario ya esta ocupado.',
       })
     }
 
@@ -133,7 +138,10 @@ export default class UserController {
       const hash = await bcrypt.hash(resultUser.data.pass, 10)
       resultUser.data.pass = hash
 
-      const newUser = await UserModel.createUserPerson({ person: resultPerson.data, user: resultUser.data })
+      const newUser = await UserModel.createUserPerson({
+        person: resultPerson.data,
+        user: resultUser.data,
+      })
       delete newUser?.person?.avatarId
 
       // aqui generaremos el token
@@ -144,31 +152,31 @@ export default class UserController {
         message: 'Usuario creada',
         data: {
           newUser,
-          rol: 'user'
+          rol: 'user',
         },
-        token
+        token,
       })
     } catch (error) {
       console.log('errorController', error)
       const errorStatus = error.status ? error.status : 500
       response.status(errorStatus).json({
         statusCode: errorStatus,
-        message: error.message
+        message: error.message,
       })
     }
   }
 
-  static async signin (request, response) {
+  static async signin(request, response) {
     const user = request.body
 
     const resultUser = validateUser({ user })
 
     if (resultUser.error) {
-      const zodError = resultUser?.error?.issues?.map(e => e.message)[0] || 'Invalid fields'
+      const zodError = resultUser?.error?.issues?.map((e) => e.message)[0] || 'Invalid fields'
       return response.status(422).json({
         statusCode: 422,
         // message: resultUser.error
-        message: zodError
+        message: zodError,
       })
     }
     const { usuario, pass } = user
@@ -178,17 +186,17 @@ export default class UserController {
       if (validUser.length === 0) {
         return response.status(404).json({
           statusCode: 404,
-          message: 'Usuario no encontrado'
+          message: 'Usuario no encontrado',
         })
       }
-      const { pass: passDB, rol } = validUser[0]
+      const { pass: passDB, rol, avatar } = validUser[0]
       const isValidPassword = await bcrypt.compare(pass, passDB)
       // const isValidPassword = passDB === pass
 
       if (!isValidPassword) {
         return response.status(404).json({
           statusCode: 401,
-          message: 'Password incorrecto'
+          message: 'Password incorrecto',
         })
       }
 
@@ -208,20 +216,21 @@ export default class UserController {
         message: 'Solicitud exitosa',
         data: {
           usuario,
-          rol
+          rol,
+          avatar,
         },
-        token
+        token,
       })
     } catch (error) {
       console.log(error)
       response.json({
         statusCode: 500,
-        message: 'Fallo al intentar logearse'
+        message: 'Fallo al intentar logearse',
       })
     }
   }
 
-  static async search (request, response) {
+  static async search(request, response) {
     try {
       const { ci } = request.params
       const result = await UserModel.search({ ci })
@@ -229,36 +238,36 @@ export default class UserController {
       if (result.length === 0) {
         response.status(404).json({
           statusCode: 404,
-          message: `No se encontro al usuario con el ci ${ci}`
+          message: `No se encontro al usuario con el ci ${ci}`,
         })
         return
       }
       response.json({
         statusCode: 200,
         message: 'Solicitud exitosa',
-        data: result
+        data: result,
       })
     } catch (error) {
       console.log(error)
       response.json({
         statusCode: 500,
-        message: 'Fallo al obtener al usuario'
+        message: 'Fallo al obtener al usuario',
       })
     }
   }
 
   // TODO: modificar este endpoint solo el usuario con su mismo id puede modificar su misma contraseña
-  static async update (request, response) {
+  static async update(request, response) {
     try {
       const user = request.body
 
       const resultUser = validatePartialUser({ user })
 
       if (resultUser.error) {
-        const zodError = resultUser?.error?.issues?.map(e => e.message)[0] || 'Invalid fields'
+        const zodError = resultUser?.error?.issues?.map((e) => e.message)[0] || 'Invalid fields'
         return response.status(422).json({
           statusCode: 422,
-          message: zodError
+          message: zodError,
         })
       }
       const { usuario, pass } = user
@@ -269,20 +278,20 @@ export default class UserController {
       if (newUser === false) {
         return response.json({
           statusCode: 404,
-          message: 'Nose ha encontrado al usuario'
+          message: 'Nose ha encontrado al usuario',
         })
       }
 
       response.json({
         statusCode: 200,
         message: 'Usuario modificada',
-        data: newUser
+        data: newUser,
       })
     } catch (error) {
       console.log(error)
       response.json({
         statusCode: 500,
-        message: 'Fallo al intentar modificar el usuario'
+        message: 'Fallo al intentar modificar el usuario',
       })
     }
   }
--- BACKEND/middlewares/uploadImage.js
+++ BACKEND/middlewares/uploadImage.js
@@ -2,12 +2,13 @@ import multer from 'multer'
 
 export const upload = multer({
   dest: 'images/',
-  fileFilter: (req, file, cb) => {
+  //fileFilter: (req, file, cb) => {
+  fileFilter: (_, file, cb) => {
     const validMIMEType = ['image/jpeg', 'image/png', 'image/webp', 'image/jpg', 'image/avif']
     if (validMIMEType.includes(file.mimetype)) {
       cb(null, true)
     } else {
       cb(new Error('InvalidTypeMulter'))
     }
-  }
+  },
 })
